b5a56a8e0861f6fdd08e3d47673da5d256101fe3
an1rudh-08 <72168090+an1rudh-08@users.noreply.github.com>
2025-07-29 17:43:23 +0530

    Update sortingHelper.java
    int + String

--- sortingHelper.java
+++ sortingHelper.java
@@ -1,15 +1,29 @@
 // SORTING int[]
-public static void sortAsc(int[] arr) {
+public static void sortA(int[] arr) {
     Arrays.sort(arr);
     // Printing Array;
     //System.out.println(Arrays.toString(arr));        
 }
 
-public static void sortDes(int[] arr) {
+public static void sortD(int[] arr) {
     Arrays.sort(arr);
     for (int l = 0, r = arr.length - 1; l < r; l++, r--) {
         int t = arr[l]; arr[l] = arr[r]; arr[r] = t;
     }
     // Printing Array;
     //System.out.println(Arrays.toString(arr));
+}
+
+// SORTING A String
+static String sortD(String str) {
+    if (str == null || str.length() <= 1) return str;
+    char[] c = str.toCharArray();
+    java.util.Arrays.sort(c);
+    return new StringBuilder(new String(c)).reverse().toString();
+}
+static String sortA(String str) {
+    if (str == null || str.length() <= 1) return str;
+    char[] c = str.toCharArray();
+    java.util.Arrays.sort(c);
+    return new StringBuilder(new String(c)).toString();
 }
